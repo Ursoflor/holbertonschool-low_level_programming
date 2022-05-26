77c7026a9c0e535581b4fc748a2b5a16b712275f
Ursoflor <martinwittb@gmail.com>
2022-05-25 21:30:52 -0700

    -s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-/* This program uses puts */
 #include <stdio.h>
-#include <string.h>
 int main(void)
 {
-char str[] = "\"Programming is like building a multilingual puzzle";
-puts(str);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
