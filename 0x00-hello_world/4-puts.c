c13420463830ff2969997cff00bf38ed3037886b
Ursoflor <martinwittb@gmail.com>
2022-05-25 21:23:25 -0700

    -s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
 return (0);
 }
