052e29a7d6086ea17a70e296d21a18fb5b2e399f
Ursoflor <martinwittb@gmail.com>
2022-05-25 21:44:00 -0700

    -s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * main - the entry point
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
