3b5b76c49e25ffecb25e6911d8e0bf0546bb87cd
Ursoflor <martinwittb@gmail.com>
2022-05-26 20:49:03 -0700

    nested for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point for program, main function
+ * Return: 0
+ **/
+int main(void)
+{
+		char ch = 'a';
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+			putchar(ch);
+		for (ch = 'A'; ch <= 'Z'; ch++)
+			putchar(ch);
+		putchar('\n');
+return (0);
+}
+
