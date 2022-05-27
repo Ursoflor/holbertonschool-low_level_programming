ac9700cf3fba97c8b94a6ba466802f1afe3ea165
Ursoflor <martinwittb@gmail.com>
2022-05-26 21:37:08 -0700

    while loop continue

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point for program, main function
+ * Return: 0
+ */
+int main(void)
+{
+        char ch = '0';
+
+        for (ch = '0'; ch <= '9'; ch++)
+                putchar(ch);
+        putchar('\n');
+	return (0);
+}
