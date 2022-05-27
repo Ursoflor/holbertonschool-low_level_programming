c26bb05cb47c278031a103148aaddab25381de6b
Ursoflor <martinwittb@gmail.com>
2022-05-26 21:25:27 -0700

    az

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *  * main - entry point for program, main function
+ *   * Return: 0
+ *    */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		ch++;
+	if (ch == 'e')
+		continue;
+	if (ch == 'q')
+		continue;
+	putchar(ch);
+	putchar('\n');
+	}
+return (0);
+}
+
